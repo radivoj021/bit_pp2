e182f7321a41fad65676a228e9a9cd5a179a7cc4
BIT Student <classroom@bgit.rs>
2019-10-17 14:36:17 +0200

    save changes

---
+++ w2d3/task1.js
@@ -0,0 +1,304 @@
+
+/* var godine = "25";
+var c = console.log
+
+if (godine < 40) {
+    c("Mozes dobiti kredit, ziveces dovoljno dugo da ga vratis");
+}
+else if (godine = 40) {
+    c("prosao si za dlaku");
+}
+else {
+    c("a gde si ti posao, vrati se nazad");
+}
+
+var a = '1';
+var result = '';
+
+switch (a) {
+    case 1:
+        result = 'Number 1';
+        break;
+    case '1':
+        result = 'String 1';
+        break;
+    default:
+        result = 'I don\'t know';
+        break;
+}
+
+
+
+var dan = 1;
+var dayName = "";
+
+switch (dan) {
+    case 1:
+        dayName = "Ponedeljak";
+        break;
+    case 2:
+        dayName = "Utorak";
+        break;
+    case 3:
+        dayName = "Sreda";
+        break;
+    case 4:
+        dayName = "Cetvrtak";
+        break;
+    case 5:
+        dayName = "Petak";
+        break;
+
+    default:
+        dayName = "Onda nije dan";
+        break;
+}
+
+console.log(dayName);
+
+
+
+var day = 10;
+var dayName = "";
+
+switch (day) {
+    case 1:
+        dayName = "Ponedeljak";
+        break;
+    case 2:
+        dayName = "Utorak";
+        break;
+    case 3:
+        dayName = "Sreda";
+        break;
+    case 4:
+        dayName = "Cetvrtak";
+        break;
+    case 5:
+        dayName = "Petak";
+        break;
+
+    default:
+        dayName = "Vrednost treba da je izmedju 1 i 7";
+        break;
+}
+
+console.log(dayName);
+
+
+
+var day = 8;
+var today = "";
+
+
+switch (day) {
+    case 1:
+        today = "weekday";
+        break;
+    case 2:
+        today = "weekday";
+        break;
+    case 3:
+        today = "weekday";
+        break;
+    case 4:
+        today = "weekday";
+        break;
+    case 5:
+        today = "weekday";
+        break;
+    case 6:
+        today = "weekend";
+        break;
+    case 7:
+        today = "weekend";
+        break;
+    default:
+        today = "You must type numbers between 1 and 7";
+        break;
+
+}
+
+console.log(today);
+
+
+var numberOfMonth = 5;
+var month = "";
+
+switch (numberOfMonth) {
+    case 1:
+        month = "January";
+        break;
+    case 2:
+        month = "February";
+        break;
+    case 3:
+        month = "March";
+        break;
+    case 4:
+        month = "April";
+        break;
+    case 5:
+        month = "May";
+        break;
+    case 6:
+        month = "June";
+        break;
+    case 7:
+        month = "July";
+        break;
+    case 8:
+        month = "August";
+        break;
+    case 9:
+        month = "September";
+        break;
+    case 10:
+        month = "October";
+        break;
+    case 11:
+        month = "November";
+        break;
+    case 12:
+        month = "December";
+        break;
+    default:
+        mointh = "Input must be a number between 1 and 12";
+
+}
+console.log(month);
+
+
+
+var month = 5;
+var season = "";
+
+switch (month) {
+    case 12:
+    case 1:
+    case 2:
+        season = "Winter";
+        break;
+    case 3:
+    case 4:
+    case 5:
+        season = "Spring";
+        break;
+    case 6:
+    case 7:
+    case 8:
+        season = "Summer";
+        break;
+    case 9:
+    case 10:
+    case 11:
+        season = "Autumn";
+        break;
+    default:
+        season = "Input must be a number between 1 and 12";
+        break;
+}
+console.log(season);
+
+
+
+var ocena = "A";
+var grade = "";
+
+switch (ocena) {
+    case "A":
+        grade = "Good job";
+        break;
+    case "B":
+        grade = "Pretty good";
+        break;
+    case "C":
+        grade = "Passed";
+        break;
+    case "D":
+        grade = "Not so good";
+        break;
+    case "F":
+        grade = "Failed";
+        break;
+    default:
+        grade = "Unknown grade";
+        break;
+
+}
+
+console.log(grade);
+
+
+var city = "Moskva";
+var country = "";
+
+switch (city) {
+    case "Moskva":
+    case "Novi Urengoj":
+    case "Minsk":
+    case "Cernobil":
+    case "Novosibir":
+        country = "Rusija";
+        break;
+    case "Padova":
+    case "Venecija":
+    case "Rim":
+    case "Bolonja":
+    case "San Djiminjano":
+        country = "Italija";
+        break;
+    case "Berlin":
+    case "Baden-Baden":
+    case "Minhen":
+    case "Stutgart":
+    case "Nimberg":
+        country = "Nemacka";
+        break;
+    case "Berlin":
+    case "Beograd":
+    case "New Now":
+    case "Nis":
+    case "Kragujevac":
+        country = "Srbija";
+        break;
+    default:
+        country = "Please choose a different city"
+        break;
+
+}
+console.log(country);
+
+
+
+
+var number = 2;
+var number1 = 7;
+var operator = "/";
+
+switch (operator) {
+    case "+":
+        result = number + number1;
+        break;
+    case "-":
+        result = number - number1;
+        break;
+    case "*":
+        result = number * number1;
+        break;
+    case "/":
+        result = number / number1;
+        break;
+    default:
+        operator = "Please choose a different operator"
+        break;
+}
+
+console.log(result);
+
+*/
+
+var numbers = [4, 12, -3, 14];
+
+console.log(numbers);
+
