ff74610b3f4ee5fb1c81914d35eea5fb051d18ac
BIT Student <classroom@bgit.rs>
2019-10-18 14:29:02 +0200

    save

---
+++ Loop/loop.js
@@ -0,0 +1,16 @@
+var i = 0;
+
+while (i < 100) {
+    i++;
+    console.log(i);
+}
+
+var sum = 0;
+var pass = 1;
+
+while (pass <= 100) {
+    sum = sum + pass;
+    pass++;
+}
+
+console.log(sum);
