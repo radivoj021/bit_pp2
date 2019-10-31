b5c3f27884341be3855e56af0538824cb7fa6c13
radivoj021 <rapic.021@gmail.com>
2019-10-31 21:49:26 +0100

    vezbe1

---
+++ Functions/mojeVezbe/funkcije1.js
@@ -0,0 +1,48 @@
+/* 1. Write a program that calculates the maximum of two given numbers. 
+
+function zbir(a, b){
+    res = a + b;
+    return res;
+}
+
+
+var z = zbir(10,19);
+console.log(z);
+
+
+
+ 2. Write a program that checks if a given number is odd. 
+
+function odd(a){
+    if(a % 2 === 0){
+        res = "number is even";
+    }
+    else{
+        res = "number is odd";
+    }
+    return res;
+}
+
+var f = odd(11);
+console.log(f);
+
+
+
+
+3. Write a program that checks if a given number is a three digit long number. */
+
+function tdn(num){
+    if (num / 10 < 1){
+
+    }
+}
+
+/* 4. Write a program that calculates an arithmetic mean of four numbers.
+
+function four(a, b, c, d){
+    var res = (a + b + c + d) / 4;
+    return res;
+}
+
+var a = four(10, 10, 10, 10);
+console.log(a); */
